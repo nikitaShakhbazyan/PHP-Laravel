3ac20f0718270c03205b1096585618f9fbdca61d
nickshakh <shakhbazyann@gmail.com>
2024-03-13 17:57:48 +0400

    Added registration and login

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -5,6 +5,13 @@
     <title>Document</title>
 </head>
 <body>
+    @auth
+    <h1>You are logged in!</h1>
+    <form action="/logout" method="POST">
+        @csrf
+        <button>Log out</button>
+    </form>
+    @else
     <div style="border : 3px solid black">
         <h2>Register</h2>
         <form action="/register" method="POST">
@@ -15,5 +22,15 @@
             <button>Register</button>
         </form>
     </div>
+    <div style="border : 3px solid black">
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input name="loginname" type="text" placeholder="name">
+            <input name="loginpassword" type="password" name="" id="" placeholder="pass"> 
+            <button>Login</button>
+        </form>
+    </div>
+    @endauth
 </body>
 </html>
